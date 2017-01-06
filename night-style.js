58911bc96ccd852ded163cce6f9a394f5d647dfd
Brenda Zhang <brenda.zhang2@gmail.com>
2017-01-06 12:46:11 -0800

    Fix issue #4: better daytime scenery, added function to move bg images

--- night-style.js
+++ night-style.js
@@ -3,6 +3,9 @@ var hours = now.getHours();
 var minutes = now.getMinutes();
 var seconds = now.getSeconds();
 
+var clock = document.getElementById('clock');
+var body = document.getElementById('main');
+
 function checkNight(h) {
     if (h < 4 || h > 18) {
     	// after 6PM or before 4 AM
@@ -13,28 +16,30 @@ function checkNight(h) {
 }
 
 function changeStyle() {
-    var clock = document.getElementById('clock');
-    var body = document.getElementById('main');
     var timeOfDay = checkNight(hours);
 
     if (timeOfDay == "night") {
         body.style.backgroundImage = "url('stars.jpg')";
         clock.style.color = "#00ff00";
+        setInterval(moveBg, 70);
         updateStyle();
     } else {
-        body.style.backgroundImage = "url('clouds.jpg')";
+        body.style.backgroundImage = "url('med_clouds.png')";
         clock.style.color = "white";
+        setInterval(moveBg, 40);
         updateStyle();
     }
 }
 
+var pos = 0;
+function moveBg() {
+    pos ++;
+    body.style.backgroundPosition = pos + "px 0px";
+}
+
 function updateStyle() {
-    if (minutes == "00" && seconds == "00") {
-        changeStyle();
-    } else {
-        // check time every hour so scenery automatically updates
-        setTimeout(changeStyle, (60*(60-minutes)+(60-seconds))*1000);
-    }
+    // check time every hour so scenery automatically updates
+    setInterval(changeStyle, (60*(60-minutes)+(60-seconds))*1000);
 }
 
 changeStyle();
