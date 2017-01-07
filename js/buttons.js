eb7ee44fe570da57baa9bb1f827ef32581046048
Brenda Zhang <brenda.zhang2@gmail.com>
2017-01-06 23:43:08 -0800

    Issue #3: add weather button functionality

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,50 @@
 // button for changing weather
 var weatherButton = document.getElementById('weather-button');
+var rain = document.getElementById('rain');
+var snow = document.getElementById('snow');
+
+var weatherTracker = 0;
+var weatherTypes = new Array("rainy", "snowy", "cloudy");
+
+weatherButton.onclick = function showWeather() {
+    if (weatherTracker == 0) {
+        smallCloud.style.backgroundImage = "none";
+        rain.style.backgroundImage = "url('images/" + weatherTypes[weatherTracker] + ".png')";
+        snow.style.backgroundImage = "none";
+        weatherTracker++;
+    } 
+    else if (weatherTracker == 1) {
+        smallCloud.style.backgroundImage = "none";
+        rain.style.backgroundImage = "none";
+        snow.style.backgroundImage = "url('images/" + weatherTypes[weatherTracker] + ".png')";
+        weatherTracker++;
+    }
+    else if (weatherTracker == 2) {
+        rain.style.backgroundImage = "none";
+        snow.style.backgroundImage = "none";
+        smallCloud.style.backgroundImage = "url('images/" + weatherTypes[weatherTracker] + ".png')";
+        weatherTracker++;
+    } else {
+        weatherTracker = 0;
+        rain.style.backgroundImage = "none";
+        snow.style.backgroundImage = "none";
+        smallCloud.style.backgroundImage = "none";
+    }
+};
+
+var rainPos = 0;
+function moveRain() {
+    rainPos += 1;
+    rain.style.backgroundPosition = "0px " + rainPos + "px";
+}
+setInterval(moveRain, 5);
+
+var snowPos = 0;
+function moveSnow() {
+    snowPos += 1;
+    snow.style.backgroundPosition = "0px " + snowPos + "px";
+}
+setInterval(moveSnow, 20);
 
 // button for changing clock color
 var colorButton = document.getElementById('color-button');
--- js/time-style.js
+++ js/time-style.js
@@ -34,7 +34,7 @@ function changeStyle() {
         shapeButton.style.boxShadow = "0px 5px gray, 0px 10px 15px rgba(0, 0, 0, .25)";
     } else { // daytime styles
         body.style.backgroundImage = "url('images/med_clouds.png')";
-        smallCloud.style.backgroundImage = "url('images/small_clouds.png')";
+        smallCloud.style.backgroundImage = "url('images/cloudy.png')";
         timeButton.innerHTML = "Switch to night";
         weatherButton.style.boxShadow = "0px 5px #5f9daf, 0px 10px 15px rgba(0, 0, 0, .25)";
         timeButton.style.boxShadow = "0px 5px #5f9daf, 0px 10px 15px rgba(0, 0, 0, .25)";
